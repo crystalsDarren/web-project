04b990f417926377f3b8539a44f4d6fa99454f5b
crystalsDarren <167780184+crystalsDarren@users.noreply.github.com>
2024-07-11 00:37:48 -0700

    Add files via upload

---
+++ Calculator/main.js
@@ -0,0 +1,47 @@
+//This is to get all keys
+const keys = document.querySelectorAll('.key');
+const display_input = document.querySelector('.display .input');
+
+const display_output = document.querySelector('.display .output');
+
+
+let input = "";
+
+for(let key of keys){
+    const value = key.dataset.key;
+    key.addEventListener('click', () => {
+        if(value == "clear"){
+            input = "";
+            display_input.innerHTML = "";
+            display_output.innerHTML = "";
+        }else if(value == "backspace"){
+            input = input.slice(0, -1);
+            display_input.innerHTML = input;
+        }else if(value == "="){
+            let result = eval(input); 
+    
+            display_output.innerHTML = result;
+        }else if(value == "brackets"){
+            if(
+                input.indexOf("(") == -1 || 
+                input.indexOf("(") !=-1 && 
+                input.indexOf(")") !=-1 && 
+                input.lastIndexOf("(") < input.lastIndexOf(")")
+            ){
+                input += "(";
+            }else if(
+              input.indexOf("(") != -1 &&
+              input.indexOf(")")== -1  ||
+              input.indexOf("(") != -1 &&
+              input.indexOf(")") != -1 &&
+              input.lastIndexOf("(") > input.lastIndexOf(")")
+            ){
+              input -= ")"
+            }
+            display_input.innerHTML = input;
+        }else{
+            input += value;
+            display_input.innerHTML = input;
+        }
+    })
+}
